056297001a39e8a41e8bd3c7e1c62505c1e7f069
Mohamed Kamal <medoking91@gmail.com>
2023-12-15 21:58:06 +0200

    feat: diacritic extraction

--- src/encoder/arabic_encoder.py
+++ src/encoder/arabic_encoder.py
@@ -31,16 +31,31 @@ class ArabicEncoder:
         text = "".join([c for c in text if c in valid_chars])
         return re.sub(r"\s+", " ", text).strip()
 
+    def normalize_diacritic(self, diacritics: list[str]):
+        reverse_diacritic = "".join(reversed(diacritics))
+        normal_diacritic = "".join(diacritics)
+        # check both normal and reverse diacritics
+        if normal_diacritic in DIACRITICS:
+            return normal_diacritic
+        if reverse_diacritic in DIACRITICS:
+            return reverse_diacritic
+        raise ValueError(f"{diacritics} list not known diacritic")
+
     def extract_diacritics(self, text: str):
-        stack = []
+        current_diacritics = []
         diacritics = []
         chars = []
-        for c in text:
-            if c in self.diacritics:
-                stack.append(c)
+        for char in text:
+            if char in DIACRITICS:
+                current_diacritics.append(char)
             else:
-                chars.append(c)
-                if stack:
-                    diacritics.append(stack.pop())
+                diacritics.append(self.normalize_diacritic(current_diacritics))
+                chars.append(char)
+                current_diacritics = []
 
-        return "".join(chars), "".join(diacritics)
+        if len(diacritics):
+            del diacritics[0]
+
+        diacritics.append(self.normalize_diacritic(current_diacritics))
+
+        return text, chars, diacritics
